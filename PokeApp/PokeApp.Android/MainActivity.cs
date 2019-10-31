baf91671f0b766f51b4c0d7e95941dd428d1e729
Kainã Almeida <kainafa@hotmail.com>
2019-10-31 12:30:33 -0300

    KA - Splashscreen android

--- PokeApp/PokeApp.Android/MainActivity.cs
+++ PokeApp/PokeApp.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Prism.Ioc;
 
 namespace PokeApp.Droid
 {
-    [Activity(Label = "PokeApp", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "PokeApp", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
